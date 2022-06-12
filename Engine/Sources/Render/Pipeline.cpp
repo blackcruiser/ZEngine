efa8a3778fc6c44253bc328b91d0495cb7db3b96
Porco <zmzxlw@163.com>
2022-06-12 14:55:27 +0800

    [Feature] Initial Commit Draw Triangle.

---
+++ Engine/Sources/Render/Pipeline.cpp
@@ -0,0 +1,7 @@
+#include "Pipeline.h"
+
+#include <vulkan/vulkan.h>
+
+Pipeline::Pipeline() : _pipeline(VK_NULL_HANDLE)
+{
+}
---
+++ Engine/Sources/Render/Public/Pipeline.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+class Pipeline
+{
+public:
+	Pipeline();
+
+
+private:
+	VkPipeline _pipeline;
+};
