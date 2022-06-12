914acfe0b618f3a24a3b8e730ec04d179b957bf4
Porco <zmzxlw@163.com>
2022-06-12 15:50:13 +0800

    [Feature] Add TEWindow and TEDevice.

--- Engine/Sources/Window.cpp
+++ Engine/Sources/Window.cpp
@@ -1,1 +1,33 @@
 #include "Window.h"
+
+#include <stdexcept>
+
+TEWindow::TEWindow(VkInstance vkInstance) : _vkInstance(vkInstance),
+                                            glfwWindow(nullptr), vkSurface(VK_NULL_HANDLE)
+{
+}
+
+void TEWindow::Init(const std::string &appName, int width, int height)
+{
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindow = glfwCreateWindow(width, height, appName.c_str(), nullptr, nullptr);
+
+    if (glfwWindow == nullptr)
+    {
+        throw std::runtime_error("failed to create window!");
+        return;
+    }
+
+    if (glfwCreateWindowSurface(_vkInstance, glfwWindow, nullptr, &vkSurface) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create window surface!");
+    }
+}
+
+void TEWindow::Cleanup()
+{
+    vkDestroySurfaceKHR(_vkInstance, vkSurface, nullptr);
+
+    glfwDestroyWindow(glfwWindow);
+}
