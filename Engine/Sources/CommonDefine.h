4e0b04c5bbfe683e8a91609c0f8fc02f5e54f498
Porco <zmzxlw@163.com>
2022-06-15 23:23:33 +0800

    [Feature] Using Customize abbreviation type.

--- Engine/Sources/CommonDefine.h
+++ Engine/Sources/CommonDefine.h
@@ -1,1 +1,17 @@
+#pragma once
+
 #define TOYENGINE_DEBUG 1
+
+#ifndef TOYENGINE_DEBUG
+    #define TOYENGINE_RELEASE
+#endif
+
+#include <memory>
+#include <vector>
+#include <map>
+
+template<typename T>
+using TEPtr = std::shared_ptr<T>;
+
+template<typename T>
+using TEPtrArr = std::vector<std::shared_ptr<T>>;
