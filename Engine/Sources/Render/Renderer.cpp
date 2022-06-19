1b488e2181a163daec87697613a5f09de446393f
Porco <zmzxlw@163.com>
2022-06-19 14:54:09 +0800

    [Feature] Add Surface GPU class, move functions in renderer to appropriate place.

--- Engine/Sources/Render/Renderer.cpp
+++ Engine/Sources/Render/Renderer.cpp
@@ -2,6 +2,9 @@
 #include "Scene.h"
 #include "Object.h"
 #include "Material.h"
+#include "Surface.h"
+#include "CommandPool.h"
+#include "CommandBuffer.h"
 
 #include <stdexcept>
 #include <algorithm>
@@ -29,49 +32,24 @@ static std::vector<char> readFile(const std::string &filename)
     return buffer;
 }
 
-void TEForwardRenderer::Init(TEPtr<TEDevice> device, TEPtr<TEWindow> window)
+TEForwardRenderer::TEForwardRenderer(TEPtr<TEDevice> device, TEPtr<TESurface> surface) : _device(device), _surface(surface), _stagingBuffer(VK_NULL_HANDLE), _stagingBufferSize(0), _vertexBuffer(VK_NULL_HANDLE), _vertexBufferSize(0)
 {
-    _device = device;
-    _window = window;
+}
 
-    SelectSurfaceFormat();
-
-    _vkRenderPass = _device->CreateRenderPass(_vkSurfaceFormat.format);
+void TEForwardRenderer::Init()
+{
+    VkSurfaceFormatKHR surfaceFormat = _surface->GetSurfaceFormat();
+    _vkRenderPass = _device->CreateRenderPass(surfaceFormat.format);
     CreateSwapchain(_vkRenderPass);
 
-    _vkCommandPool = _device->CreateCommandPool(_device->graphicQueueFamilyIndex);
-    _vkCommandBuffer = _device->CreateCommandBuffer(_vkCommandPool);
+    _commandPool = _device->CreateCommandPool(_device);
+    _commandBuffer = _commandPool->CreateCommandBuffer(_commandPool);
 
     _imageAvailableSemaphore = _device->CreateSemaphore();
     _renderFinishedSemaphore = _device->CreateSemaphore();
 
     _inFlightFence = _device->CreateFence(true);
 }
-
-void TEForwardRenderer::SelectSurfaceFormat()
-{
-    uint32_t formatCount;
-    vkGetPhysicalDeviceSurfaceFormatsKHR(_device->vkPhysicalDevice, _window->vkSurface, &formatCount, nullptr);
-
-    std::vector<VkSurfaceFormatKHR> surfaceFormats;
-    if (formatCount != 0)
-    {
-        surfaceFormats.resize(formatCount);
-        vkGetPhysicalDeviceSurfaceFormatsKHR(_device->vkPhysicalDevice, _window->vkSurface, &formatCount, surfaceFormats.data());
-    }
-
-    for (size_t i = 0; i < surfaceFormats.size(); i++)
-    {
-        const VkSurfaceFormatKHR &currentSurfaceFormat = surfaceFormats[i];
-
-        if (currentSurfaceFormat.colorSpace == VkColorSpaceKHR::VK_COLOR_SPACE_SRGB_NONLINEAR_KHR && currentSurfaceFormat.format == VkFormat::VK_FORMAT_B8G8R8A8_SRGB)
-        {
-            _vkSurfaceFormat = currentSurfaceFormat;
-            break;
-        }
-    }
-}
-
 
 VkPipeline TEForwardRenderer::CreatePipeline(TEPtr<TEMaterial> material)
 {
@@ -85,68 +63,30 @@ VkPipeline TEForwardRenderer::CreatePipeline(TEPtr<TEMaterial> material)
     _vkShaderModules.push_back(vkFragmentShaderModule);
 
     _vkPipelineLayout = _device->CreatePipelineLayout();
-    VkPipeline vkPipeline = _device->CreateGraphicPipeline(vkVerterShaderModule, vkFragmentShaderModule, _vkExtent, _vkPipelineLayout, _vkRenderPass);
+    VkPipeline vkPipeline = _device->CreateGraphicPipeline(vkVerterShaderModule, vkFragmentShaderModule, _surface->GetExtent(), _vkPipelineLayout, _vkRenderPass);
 
     return vkPipeline;
 }
 
 void TEForwardRenderer::CreateSwapchain(VkRenderPass renderPass)
 {
-    // Swap Chain
-    uint32_t presentModeCount;
-    vkGetPhysicalDeviceSurfacePresentModesKHR(_device->vkPhysicalDevice, _window->vkSurface, &presentModeCount, nullptr);
+    VkSurfaceFormatKHR vkSurfaceFormat = _surface->GetSurfaceFormat();
+    VkPresentModeKHR vkPresentMode = _surface->GetPresentMode();
+    VkSurfaceCapabilitiesKHR vkCapabilities = _surface->GetCpabilities();
+    VkExtent2D extent = _surface->GetExtent();
 
-    std::vector<VkPresentModeKHR> presentModes;
-    if (presentModeCount != 0)
-    {
-        presentModes.resize(presentModeCount);
-        vkGetPhysicalDeviceSurfacePresentModesKHR(_device->vkPhysicalDevice, _window->vkSurface, &presentModeCount, presentModes.data());
-    }
-
-    VkPresentModeKHR presentMode{};
-    for (size_t i = 0; i < presentModes.size(); i++)
-    {
-        const VkPresentModeKHR &currentPresentMode = presentModes[i];
-
-        if (currentPresentMode == VkPresentModeKHR::VK_PRESENT_MODE_MAILBOX_KHR)
-        {
-            presentMode = currentPresentMode;
-            break;
-        }
-    }
-
-    VkSurfaceCapabilitiesKHR capabilities{};
-    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(_device->vkPhysicalDevice, _window->vkSurface, &capabilities);
-
-    // GLFW uses two units when measuring sizes: pixels and screen coordinates.
-
-    if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
-    {
-        _vkExtent = capabilities.currentExtent;
-    }
-    else
-    {
-
-        // glfwGetFramebufferSize: resolution of the window in pixel
-        int framebufferWidth, framebufferHeight;
-        glfwGetFramebufferSize(_window->glfwWindow, &framebufferWidth, &framebufferHeight);
-
-        _vkExtent.width = std::clamp(static_cast<uint32_t>(framebufferWidth), capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
-        _vkExtent.height = std::clamp(static_cast<uint32_t>(framebufferHeight), capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
-    }
-
-    _vkSwapchain = _device->CreateSwapchain(capabilities.minImageCount + 1, _vkSurfaceFormat.format, _vkSurfaceFormat.colorSpace, _vkExtent, capabilities.currentTransform, presentMode);
+    _vkSwapchain = _device->CreateSwapchain(vkCapabilities.minImageCount + 1, vkSurfaceFormat.format, vkSurfaceFormat.colorSpace, extent, vkCapabilities.currentTransform, vkPresentMode);
 
     uint32_t imageCount;
-    vkGetSwapchainImagesKHR(_device->vkDevice, _vkSwapchain, &imageCount, nullptr);
+    vkGetSwapchainImagesKHR(_device->GetRawDevice(), _vkSwapchain, &imageCount, nullptr);
     _vkImages.resize(imageCount);
-    vkGetSwapchainImagesKHR(_device->vkDevice, _vkSwapchain, &imageCount, _vkImages.data());
+    vkGetSwapchainImagesKHR(_device->GetRawDevice(), _vkSwapchain, &imageCount, _vkImages.data());
 
     // Image View
     _vkImageViews.resize(imageCount);
     for (size_t i = 0; i < imageCount; i++)
     {
-        _vkImageViews[i] = _device->CreateImageView(_vkImages[i], _vkSurfaceFormat.format);
+        _vkImageViews[i] = _device->CreateImageView(_vkImages[i], vkSurfaceFormat.format);
     }
 
     _vkFramebuffers.resize(imageCount);
@@ -155,7 +95,7 @@ void TEForwardRenderer::CreateSwapchain(VkRenderPass renderPass)
         std::vector<VkImageView> attachments = {
             _vkImageViews[i]};
 
-        _vkFramebuffers[i] = _device->CreateFramebuffer(renderPass, attachments, _vkExtent.width, _vkExtent.height);
+        _vkFramebuffers[i] = _device->CreateFramebuffer(renderPass, attachments, extent.width, extent.height);
     }
 }
 
@@ -177,47 +117,38 @@ void TEForwardRenderer::GatherObjects(TEPtr<TEScene> scene)
 
 void TEForwardRenderer::CopyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size)
 {
-    VkCommandBufferAllocateInfo allocInfo{};
-    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    allocInfo.commandPool = _vkCommandPool;
-    allocInfo.commandBufferCount = 1;
+    TECommandBuffer *commandBuffer = _commandPool->CreateCommandBuffer(_commandPool);
+    VkCommandBuffer vkCommandBuffer = commandBuffer->GetRawCommandBuffer();
 
-    VkCommandBuffer commandBuffer;
-    vkAllocateCommandBuffers(_device->vkDevice, &allocInfo, &commandBuffer);
-
-    VkCommandBufferBeginInfo beginInfo{};
-    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-
-    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+    commandBuffer->Begin();
 
     VkBufferCopy copyRegion{};
     copyRegion.size = size;
-    vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+    vkCmdCopyBuffer(vkCommandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
 
-    vkEndCommandBuffer(commandBuffer);
+    commandBuffer->End();
 
     VkSubmitInfo submitInfo{};
     submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &commandBuffer;
+    submitInfo.pCommandBuffers = &vkCommandBuffer;
 
-    vkQueueSubmit(_device->vkGraphicQueue, 1, &submitInfo, VK_NULL_HANDLE);
-    vkQueueWaitIdle(_device->vkGraphicQueue);
+    vkQueueSubmit(_device->GetGraphicQueue(), 1, &submitInfo, VK_NULL_HANDLE);
+    vkQueueWaitIdle(_device->GetGraphicQueue());
 
-    vkFreeCommandBuffers(_device->vkDevice, _vkCommandPool, 1, &commandBuffer);
+    _commandPool->DestroyCommandBuffer(commandBuffer);
 }
 
 void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
 {
     GatherObjects(scene);
 
-    vkWaitForFences(_device->vkDevice, 1, &_inFlightFence, VK_TRUE, UINT64_MAX);
-    vkResetFences(_device->vkDevice, 1, &_inFlightFence);
+    VkDevice vkDevice = _device->GetRawDevice();
+    vkWaitForFences(vkDevice, 1, &_inFlightFence, VK_TRUE, UINT64_MAX);
+    vkResetFences(vkDevice, 1, &_inFlightFence);
 
     uint32_t imageIndex;
-    vkAcquireNextImageKHR(_device->vkDevice, _vkSwapchain, UINT64_MAX, _imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
+    vkAcquireNextImageKHR(vkDevice, _vkSwapchain, UINT64_MAX, _imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
 
     VkCommandBufferBeginInfo beginInfo{};
     beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
@@ -227,18 +158,16 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
     renderPassInfo.renderPass = _vkRenderPass;
     renderPassInfo.framebuffer = _vkFramebuffers[imageIndex];
     renderPassInfo.renderArea.offset = {0, 0};
-    renderPassInfo.renderArea.extent = _vkExtent;
+    renderPassInfo.renderArea.extent = _surface->GetExtent();
 
     VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
     renderPassInfo.clearValueCount = 1;
     renderPassInfo.pClearValues = &clearColor;
 
-    if (vkBeginCommandBuffer(_vkCommandBuffer, &beginInfo) != VK_SUCCESS)
-    {
-        throw std::runtime_error("failed to begin recording command buffer!");
-    }
+    VkCommandBuffer vkCommandBuffer = _commandBuffer->GetRawCommandBuffer();
+    _commandBuffer->Begin();
 
-    vkCmdBeginRenderPass(_vkCommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBeginRenderPass(vkCommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
     for (auto &pair : _objectsToRender)
     {
@@ -270,8 +199,9 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
 
         if (_stagingBuffer != VK_NULL_HANDLE && totalBufferSize > _stagingBufferSize)
         {
-            vkFreeMemory(_device->vkDevice, _stagingBufferMemory, nullptr);
-            vkDestroyBuffer(_device->vkDevice, _stagingBuffer, nullptr);
+            _device->FreeMemmory(_stagingBufferMemory);
+            _device->DestroyBuffer(_stagingBuffer);
+
             _stagingBuffer = VK_NULL_HANDLE;
         }
 
@@ -284,8 +214,9 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
 
         if (_vertexBuffer != VK_NULL_HANDLE && totalBufferSize > _vertexBufferSize)
         {
-            vkFreeMemory(_device->vkDevice, _vertexBufferMemory, nullptr);
-            vkDestroyBuffer(_device->vkDevice, _vertexBuffer, nullptr);
+            _device->FreeMemmory(_vertexBufferMemory);
+            _device->DestroyBuffer(_vertexBuffer);
+
             _vertexBuffer = VK_NULL_HANDLE;
         }
 
@@ -293,11 +224,10 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
         {
             _vertexBuffer = _device->CreateBuffer(totalBufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
             _vertexBufferMemory = _device->AllocateAndBindBufferMemory(_vertexBuffer, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-            _vertexBufferSize = totalBufferSize;
         }
 
         void *data = nullptr;
-        Result = vkMapMemory(_device->vkDevice, _stagingBufferMemory, 0, totalBufferSize, 0, &data);
+        Result = vkMapMemory(_device->GetRawDevice(), _stagingBufferMemory, 0, totalBufferSize, 0, &data);
         if (Result != VK_SUCCESS)
         {
             throw std::runtime_error("failed to map memory!");
@@ -313,26 +243,22 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
             vertexCounts += object->vertices.size();
         }
 
-        vkUnmapMemory(_device->vkDevice, _stagingBufferMemory);
+        vkUnmapMemory(_device->GetRawDevice(), _stagingBufferMemory);
 
         CopyBuffer(_stagingBuffer, _vertexBuffer, totalBufferSize);
 
-        vkCmdBindPipeline(_vkCommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, vkPipeline);
+        vkCmdBindPipeline(vkCommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, vkPipeline);
 
         VkBuffer vertexBuffers[] = {_vertexBuffer};
         VkDeviceSize offsets[] = {0};
 
-        vkCmdBindVertexBuffers(_vkCommandBuffer, 0, 1, vertexBuffers, offsets);
-        vkCmdDraw(_vkCommandBuffer, vertexCounts, 1, 0, 0);
+        vkCmdBindVertexBuffers(vkCommandBuffer, 0, 1, vertexBuffers, offsets);
+        vkCmdDraw(vkCommandBuffer, vertexCounts, 1, 0, 0);
     }
 
-    vkCmdEndRenderPass(_vkCommandBuffer);
+    vkCmdEndRenderPass(vkCommandBuffer);
 
-    VkResult Result = vkEndCommandBuffer(_vkCommandBuffer);
-    if (Result != VK_SUCCESS)
-    {
-        throw std::runtime_error("failed to record command buffer!");
-    }
+    _commandBuffer->End();
 
     VkSemaphore waitSemaphores[] = {_imageAvailableSemaphore};
     VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
@@ -343,11 +269,11 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
     submitInfo.pWaitSemaphores = waitSemaphores;
     submitInfo.pWaitDstStageMask = waitStages;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &_vkCommandBuffer;
+    submitInfo.pCommandBuffers = &vkCommandBuffer;
     submitInfo.signalSemaphoreCount = 1;
     submitInfo.pSignalSemaphores = signalSemaphores;
 
-    if (vkQueueSubmit(_device->vkGraphicQueue, 1, &submitInfo, _inFlightFence) != VK_SUCCESS)
+    if (vkQueueSubmit(_device->GetGraphicQueue(), 1, &submitInfo, _inFlightFence) != VK_SUCCESS)
     {
         throw std::runtime_error("failed to submit draw command buffer!");
     }
@@ -362,46 +288,45 @@ void TEForwardRenderer::RenderFrame(TEPtr<TEScene> scene)
     presentInfo.pImageIndices = &imageIndex;
     presentInfo.pResults = nullptr; // Optional
 
-    vkQueuePresentKHR(_device->vkPresentQueue, &presentInfo);
+    vkQueuePresentKHR(_device->GetPresentQueue(), &presentInfo);
 }
 
 void TEForwardRenderer::Cleanup()
 {
+    _device->FreeMemmory(_vertexBufferMemory);
+    _device->DestroyBuffer(_vertexBuffer);
 
-    vkFreeMemory(_device->vkDevice, _vertexBufferMemory, nullptr);
-    vkDestroyBuffer(_device->vkDevice, _vertexBuffer, nullptr);
+    _device->FreeMemmory(_stagingBufferMemory);
+    _device->DestroyBuffer(_stagingBuffer);
 
-    vkFreeMemory(_device->vkDevice, _stagingBufferMemory, nullptr);
-    vkDestroyBuffer(_device->vkDevice, _stagingBuffer, nullptr);
+    _commandBuffer->Cleanup();
+    _commandPool->Cleanup();
 
-    vkFreeCommandBuffers(_device->vkDevice, _vkCommandPool, 1, &_vkCommandBuffer);
-    vkDestroyCommandPool(_device->vkDevice, _vkCommandPool, nullptr);
+    _device->DestroySemaphore(_imageAvailableSemaphore);
+    _device->DestroySemaphore(_renderFinishedSemaphore);
+    _device->DestroyFence(_inFlightFence);
 
-    vkDestroySemaphore(_device->vkDevice, _imageAvailableSemaphore, nullptr);
-    vkDestroySemaphore(_device->vkDevice, _renderFinishedSemaphore, nullptr);
-    vkDestroyFence(_device->vkDevice, _inFlightFence, nullptr);
-
-    vkDestroyPipelineLayout(_device->vkDevice, _vkPipelineLayout, nullptr);
+    _device->DestroyPipelineLayout(_vkPipelineLayout);
 
     for (auto iter = _pipelines.begin(); iter != _pipelines.end(); iter++)
-        vkDestroyPipeline(_device->vkDevice, iter->second, nullptr);
+        _device->DestroyPipeline(iter->second);
 
-    vkDestroyRenderPass(_device->vkDevice, _vkRenderPass, nullptr);
+    _device->DestroyRenderPass(_vkRenderPass);
 
     for (VkShaderModule &shaderModule : _vkShaderModules)
     {
-        vkDestroyShaderModule(_device->vkDevice, shaderModule, nullptr);
+        _device->DestroyShaderModule(shaderModule);
     }
 
     for (auto imageView : _vkImageViews)
     {
-        vkDestroyImageView(_device->vkDevice, imageView, nullptr);
+        _device->DestroyImageView(imageView);
     }
 
     for (VkFramebuffer &framebuffer : _vkFramebuffers)
     {
-        vkDestroyFramebuffer(_device->vkDevice, framebuffer, nullptr);
+        _device->DestroyFramebuffer(framebuffer);
     }
 
-    vkDestroySwapchainKHR(_device->vkDevice, _vkSwapchain, nullptr);
+    _device->DestroySwapchain(_vkSwapchain);
 }
---
+++ Engine/Sources/Surface.h
@@ -0,0 +1,38 @@
+#include "CoreDefines.h"
+
+#include <vulkan/vulkan.h>
+
+class TEGPU;
+class TEWindow;
+
+class TESurface
+{
+public:
+    TESurface(VkInstance vkInstance, TEPtr<TEGPU> GPU, TEPtr<TEWindow> window);
+    void Init();
+    void Cleanup();
+
+    VkSurfaceKHR GetRawSurface();
+    VkSurfaceFormatKHR GetSurfaceFormat();
+    VkPresentModeKHR GetPresentMode();
+    VkExtent2D GetExtent();
+
+    std::vector<VkSurfaceFormatKHR> GetSupportedSurfaceFormats();
+    std::vector<VkPresentModeKHR> GetSupportedPresentModes();
+    VkSurfaceCapabilitiesKHR GetCpabilities();
+
+private:
+    void SelectSurfaceFormat();
+    void SelectPresentMode();
+    void CalculateExtent();
+
+private:
+    VkInstance _vkInstance;
+    VkSurfaceKHR _vkSurface;
+    VkSurfaceFormatKHR _vkSurfaceFormat;
+    VkPresentModeKHR _presentMode;
+    VkExtent2D _vkExtent;
+
+    TEPtr<TEGPU> _GPU;
+    TEPtr<TEWindow> _window;
+};
