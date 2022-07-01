38dd153e8185d64933f7ec8da418d66f4fad9548
Porco <zmzxlw@163.com>
2022-07-01 22:05:42 +0800

    [Feature] Add ScriptComponent for users to write scripts. Add Sample CameraControlComponent to listen mouse move event. Modify SceneObject to support GetComponents with Base Class Type.

---
+++ Engine/Sources/Scene/ScriptComponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreDefines.h"
+#include "SceneComponent.h"
+
+class TEScriptComponent : public TESceneComponent
+{
+public:
+    TEScriptComponent();
+    ~TEScriptComponent();
+};
