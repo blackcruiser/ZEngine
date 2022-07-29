1b4ada4dda1ac11c5f94ee51bcb37dcf4aa1c22c
Porco <zmzxlw@163.com>
2022-07-29 22:08:55 +0800

    [Feature] Add obj mesh loader support.

--- Engine/Shaders/LocalToClipSpaceVertexShader.glsl
+++ Engine/Shaders/LocalToClipSpaceVertexShader.glsl
@@ -1,6 +1,9 @@
 #version 450
 
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 tecCoord;
+
 layout(location = 0) out vec3 fragColor;
 
 layout(set = 0, binding = 0) uniform UniformBuffer
